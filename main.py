1f9ded053fcaa6f32303ef290d72a77a65532f81
aatif-dawawala <aatifdawawala@gmail.com>
2025-06-05 20:26:03 -0500

    Add session logging, update readme.

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from pyfiglet import Figlet
 import argparse
 import sqlite3
 from datetime import date
+import matplotlib.pyplot as plt 
+import numpy as np
 
 parser = argparse.ArgumentParser(
     prog="Command-Line Pomodoro Timer",
@@ -15,7 +17,8 @@ parser = argparse.ArgumentParser(
 parser.add_argument('-s', '--study', help="the amount of study time in minutes")
 parser.add_argument('-r', '--rest', help="the amount of rest time in minutes")
 parser.add_argument('-ss', '--sessions', help="the number of sessions")
-parser.add_argument('-rr', '--record', action="store_true", help="enables sesssion recording")
+parser.add_argument('-l', '--log', action="store_true", help="logs the session once completed")
+parser.add_argument('-g', '--graph', action="store_true", help="shows a graph of date and study time, either immediately if passed with no other arguments, or after the sessions have been completed")
 
 args = parser.parse_args()
 
@@ -23,71 +26,73 @@ studyTime = args.study
 breakTime = args.rest
 sessionCount = args.sessions
 recordEnable = args.record
+graphEnable = args.graph
 
-if args.study is None or args.rest is None or args.sessions is None:
+if ((not graphEnable) and (studyTime is None or breakTime is None or sessionCount is None)) or ((graphEnable) and (studyTime is None or breakTime is None or sessionCount is None)):
     print("You are missing 1 or more arguments. Use the -h flag for help")
     quit()
 
-try:
-    studyTime = int(studyTime)
-    breakTime = int(breakTime)
-    sessionCount = int(sessionCount)
-except:
-    print("Make sure all your arguments are whole numbers!")
-    quit()
+if (not graphEnable) or ((studyTime is None or breakTime is None or sessionCount is None) or (not (studyTime is None or breakTime is None or sessionCount is None))):
+    try:
+        studyTime = int(studyTime)
+        breakTime = int(breakTime)
+        sessionCount = int(sessionCount)
+    except:
+        print("Make sure all your arguments are whole numbers!")
+        quit()
 
-if studyTime <= 0 or breakTime <= 0 or sessionCount <= 0:
-    print("Make sure all your arguments are positive numbers!")
-    quit()
+    if studyTime <= 0 or breakTime <= 0 or sessionCount <= 0:
+        print("Make sure all your arguments are positive numbers!")
+        quit()
 
 
-if studyTime <= 60: 
-    studyTimeCmd = f"timer {studyTime}m"
-    studySleepTime = studyTime * 60
-if studyTime > 60:
-    studyHours = studyTime / 60
-    studyHours = math.trunc(studyHours)
+    if studyTime <= 60: 
+        studyTimeCmd = f"timer {studyTime}m"
+        studySleepTime = studyTime * 60
+    if studyTime > 60:
+        studyHours = studyTime / 60
+        studyHours = math.trunc(studyHours)
 
-    studyMinutes = studyTime % 60
+        studyMinutes = studyTime % 60
 
-    studySleepTime = (studyMinutes * 60) + (studyHours * (60 ** 2))
-    if studyMinutes == 0:  
-        studyTimeCmd = f"timer {studyHours}h"
-    else:  
-        studyTimeCmd = f"timer {studyHours}h{studyMinutes}m"
-    
-if breakTime <= 60: 
-    breakTimeCmd = f"timer {breakTime}m"
-    breakSleepTime = breakTime * 60
-if breakTime > 60:
-    breakHours = breakTime / 60
-    breakHours = math.trunc(breakHours)
+        studySleepTime = (studyMinutes * 60) + (studyHours * (60 ** 2))
+        if studyMinutes == 0:  
+            studyTimeCmd = f"timer {studyHours}h"
+        else:  
+            studyTimeCmd = f"timer {studyHours}h{studyMinutes}m"
+        
+    if breakTime <= 60: 
+        breakTimeCmd = f"timer {breakTime}m"
+        breakSleepTime = breakTime * 60
+    if breakTime > 60:
+        breakHours = breakTime / 60
+        breakHours = math.trunc(breakHours)
 
-    breakMinutes = breakTime % 60
+        breakMinutes = breakTime % 60
 
-    breakSleepTime = (breakMinutes * 60) + (breakHours * (60 ** 2))
-    if breakMinutes == 0:  
-        studyTimeCmd = f"timer {breakHours}h"
-    else:  
-        studyTimeCmd = f"timer {breakHours}h{studyMinutes}m"
+        breakSleepTime = (breakMinutes * 60) + (breakHours * (60 ** 2))
+        if breakMinutes == 0:  
+            studyTimeCmd = f"timer {breakHours}h"
+        else:  
+            studyTimeCmd = f"timer {breakHours}h{studyMinutes}m"
 
-time.sleep(1)
+    time.sleep(1)
 
-for i in range(sessionCount):
-    timer = subprocess.Popen(f"{studyTimeCmd} -m \"Study session {i + 1}/{sessionCount}\"", shell=True)
-    time.sleep(studySleepTime)
+    for i in range(sessionCount):
+        timer = subprocess.Popen(f"{studyTimeCmd} -m \"Study session {i + 1}/{sessionCount}\"", shell=True)
+        time.sleep(studySleepTime)
 
-    timer.terminate()
-    os.system("clear")
+        timer.terminate()
+        os.system("clear")
 
-    timer = subprocess.Popen(f"{breakTimeCmd} -m \"Break {i + 1}/{sessionCount}\"", shell=True)
-    time.sleep(breakSleepTime)
+        timer = subprocess.Popen(f"{breakTimeCmd} -m \"Break {i + 1}/{sessionCount}\"", shell=True)
+        time.sleep(breakSleepTime)
 
-    timer.terminate()
-    os.system("clear")
+        timer.terminate()
+        os.system("clear")
 
-f = Figlet(font='slant')
-print(f.renderText('You did it!'))
+    f = Figlet(font='slant')
+    print(f.renderText('You did it!'))
 
 if recordEnable:
     today = date.today()
@@ -103,7 +108,35 @@ if recordEnable:
     ''')
     cursor.execute(f'''
             INSERT INTO sessions 
-            VALUES ('{formatted_date}', {studyTime});
+            VALUES ('{formatted_date}', {studyTime * sessionCount});
     ''')
     dbConn.commit()
 
+if graphEnable:
+    dbConn = sqlite3.connect("studylog.db")
+    cursor = dbConn.cursor()
+
+    cursor.execute("SELECT date FROM sessions")      
+    dates = cursor.fetchall()
+
+    cursor.execute("SELECT timestudied FROM sessions")      
+    times = cursor.fetchall()
+
+    xAxis = []
+    yAxis = []
+
+    for i in range(len(dates)):
+        data = str(dates[i])
+        data = data[2:(len(data) - 3)]
+        xAxis.append(data)
+
+        data = str(times[i])
+        data = int(data[1:(len(data)) - 2])
+        yAxis.append(data)
+
+
+    fig = plt.figure()
+    fig, ax = plt.subplots()
+    ax.plot(xAxis, yAxis)
+
+    plt.show()
